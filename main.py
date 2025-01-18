a303392f6723f98e96c945fc5c87a0cd75af2815
kaiwxn <kwyang0620@icloud.com>
2025-01-18 15:30:17 +0100

    positioning of buttons

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from src.util import Util
 from src.map import Map
 from src.object import Object
 from src.constants import Constants
+from src.button import Button
 
 # setup window 
 pygame.display.set_caption("Pathfinder GUI")
@@ -18,11 +19,7 @@ pygame.display.set_icon(pygame.image.load(os.path.join(os.getcwd(), "img", "icon
 map1 = Map()
 util = Util()
 fpsClock = pygame.time.Clock()
-manager = pygame_gui.UIManager((Constants.SCREEN_HEIGHT, Constants.SCREEN_WIDTH), theme_path=os.path.join(os.getcwd(), "src/themes/themes.json"))
 
-# ############################## #
-#  Define button functions here  #
-# ############################## #
 
 def buttonOutputfunc():
     util.printCommands(map1)
@@ -30,7 +27,6 @@ def buttonOutputfunc():
 def buttonClearfunc():
     global map1
     map1.clear_map()
-    del map1
     map1 = Map()
 
 def buttonSwitchRightfunc():
@@ -41,41 +37,10 @@ def buttonSwitchLeftfunc():
 
 
 
-# ############################## #
-#   Define button objects here   #
-# ############################## #
-
-containerButton = pygame_gui.elements.UIPanel(relative_rect=pygame.Rect((20, 50), (20, 20)),
-                                        starting_height=1,
-                                        manager=manager,anchors={'left':'left', 'right':'right', 'top':'top', 'bottom':'bottom'})
-containerButton.set_visual_debug_mode(True)
-
-
-buttonSwitchLeft = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(10, 100, 85, 30),
-                                                text='Left',
-                                                manager=manager,
-                                                container=containerButton)
-
-buttonSwitchRight = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(105, 100, 85, 30),
-                                                 text='Right',
-                                                 manager=manager,
-                                                 container=containerButton)
-
-buttonOutput = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(105, 100, 85, 30),
-                                                 text='Right',
-                                                 manager=manager,
-                                                 container=containerButton)
-
-
-# selectionSide = pygame_gui.elements.UISelectionList(relative_rect=pygame.Rect((350, 350), (100, 50)),
-#                                                     item_list=['Left', 'Right'],
-#                                                     manager=manager)
-
-
-# Button(1125, 20, 150, 100, 'Output', buttonOutputfunc)
-# Button(1125, 140, 65, 65, 'Left', buttonSwitchLeftfunc)
-# Button(1210, 140, 65, 65, 'Right', buttonSwitchRightfunc)
-# Button(1125, 225, 150, 65, 'Clear Map', buttonClearfunc, "red")
+Button(312, 630, 65, 62, 'Left', buttonSwitchLeftfunc)
+Button(392, 630, 65, 62, 'Right', buttonSwitchRightfunc)
+Button(472, 630, 150, 62, 'Clear Map', buttonClearfunc)
+Button(637, 630, 150, 62, 'Output', buttonOutputfunc)
 
 
 def get_events():
@@ -98,12 +63,7 @@ def get_events():
         if event.type == pygame.KEYDOWN and pygame.K_1 <= event.key <= pygame.K_6:
             map1.current_round = event.key - pygame.K_1
 
-        if event.type == pygame_gui.UI_BUTTON_PRESSED:
-          if event.ui_element == buttonOutput:
-                buttonOutputfunc()
 
-        manager.process_events(event)
-    manager.update(fpsClock.tick(c.FPS) / 1000.0)
 
 def run():
     gameloop = True
@@ -118,7 +78,6 @@ def run():
         for object in Object.objects:
             object.process()
 
-        manager.draw_ui(Object.screen)
 
         # Update 
         pygame.display.flip()
